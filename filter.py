6b94acbf50623ef7306b2f5043e7ebd020ce21dd
vsgangadhara <vsgangadhara@gmail.com>
2024-02-11 23:49:31 +0530

    filter in pyspark

--- filter.py
+++ filter.py
@@ -2,10 +2,18 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
 spark=SparkSession.builder.getOrCreate()
-data=[(1,"ganga",31),(2,"priya",28),(2,"love",2)]
-df1=spark.createDataFrame(data,("id","name","age"))
+data=[(1,"ganga",31),(2,"priya",28),(2,"love",29)]
+df=spark.createDataFrame(data,("id","name","age"))
 
-#df1=df1.filter(df1.age>28).show()
-#df1=df1.filter(df1["age"]>28).show()
-#df1=df1.filter(col("age")>28).show()
+df1=df.filter(df.age>28).show()
+df1=df.filter((df["age"]>28)&(df["age"]<=30)).show()
+df1=df.filter((df["age"]>=28)|(df["age"]<=30))
+df1=df.filter(df.age.isNull())
+df1=df.filter(df.age.isNotNull())
+df1=df.filter(df.name.like('p%')).show()
+df1=df.filter(df.name.isin("ganga")).show()
+df1=df.filter(df.name.contains('g')).show()
+df1=df.filter(df.name.startswith('p')).show()
+df1=df.filter(df.name.endswith('a')).show()
+df1=df.filter(col("age")>28).show()
 
